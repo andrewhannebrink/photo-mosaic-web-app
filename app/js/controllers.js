49619e8b47e1640b79bfba17c7eb24aec0aef770
Ubuntu <ubuntu@ip-172-31-43-151.us-west-2.compute.internal>
2015-07-14 01:11:49 +0000

    gotta eat sometimes :/

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -48,6 +48,7 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
       "upload":"img/win/syncui_135-0.png"
     },
     "emoji": {
+      "protractor": "img/emoji/1f4d0.png",
       "icon": [
         "img/emoji/1f525.png",
         "img/emoji/1f365.png",
@@ -57,7 +58,8 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
       ]
     }
   };
-  $scope.curIcon = $scope.iconSets.win.icon
+  $scope.curIcon = $scope.iconSets.emoji.icon
+  
 
   
 
@@ -70,7 +72,7 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
   };
 
 
-
+  // magical/mystical regex expression for detecting mobile devices
   $scope.mobilecheck = function() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))check = true})(navigator.userAgent||navigator.vendor||window.opera);
@@ -79,7 +81,7 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
 
   $scope.mobile = $scope.mobilecheck();
 
-
+  //sets browser style depending on mobile / web browser detection
   $scope.browserfy = function(w, h) {
     var mobile = $scope.mobilecheck();
     if (mobile === true) {
@@ -87,7 +89,7 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
         'top': '0px',
         background: 'rgba(255, 255, 255, 0.85)',
         width: '280px',
-        height: '330px',
+        height: '350px',
         position: 'fixed'
       };
       $scope.mobileWrap = {
@@ -102,15 +104,20 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
       }
     } else {
       $scope.picStyle = {
-        'top': '32px',
-        left: '120px',
+        'top': '84px',
+        left: '220px',
         width: w + 'px',
         height: h + 'px'
       }
       $scope.browserStyle = {
+        '-webkit-box-shadow': '5px 5px 6px 5px rgba(48,48,48,0.61)',
+        '-moz-box-shadow': '5px 5px 6px 5px rgba(48,48,48,0.61)',
+        'box-shadow': '5px 5px 6px 5px rgba(48,48,48,0.61)',
         padding:'16px',
-        'top': '120px',
-        left: '32px'
+        'top': '200px',
+        left: '32px',
+        'border-width': '7px',
+        'border-radius': '23px'
       };
     }
     $scope.browserChecked = true;
@@ -191,5 +198,35 @@ pApp.controller('PCtrl', ['$scope', '$http', function ($scope, $http) {
       this.matY[y] = y%10;
     }
   };
+
+  $scope.infoStyle = {
+    'z-index': '150',
+    width: '200px',
+    height: '140px'
+  };
+
+  $scope.updateInfoStyle = function() {
+    this.infoStyle['top'] = '50px';
+    this.infoStyle['left'] = '50px';
+  };
+
+  $scope.defineNewPicStyle = function(w, h) {
+    this.width = w;
+    this.height = h;
+    var b = w > h ? w : h;
+    var s = w < h ? w : h;
+    this.maxMult = 900 / b;
+    if (this.maxMult < 1) {
+      this.extMaxMult = 1000 / b;
+    }   
+    this.minMult = 32 / s;  
+    this.curWidth = this.maxMult > 1 ? w : w*this.maxMult;
+    this.curHeight = this.maxMult > 1 ? h : h*this.maxMult;
+    this.reGrid();
+    this.browserfy(this.curWidth, this.curHeight);
+    this.updateInfoStyle();
+  };
+  
+  $scope.defineNewPicStyle(1920, 1080);
 }]);
 
--- app/js/directives.js
+++ app/js/directives.js
@@ -11,20 +11,7 @@ pApp.directive('imageonload', function() {
         //TODO if w or h > 1080 then make pic smaller with picStyle
         var w = element[0].width;
         var h = element[0].height;
-        scope.width = w;
-        scope.height = h;
-        var b = w > h ? w : h;
-        var s = w < h ? w : h;
-        scope.maxMult = 900 / b;
-        if (scope.maxMult < 1) {
-          scope.extMaxMult = 1000 / b;
-        }
-        scope.minMult = 32 / s; 
-        scope.curWidth = scope.maxMult > 1 ? w : w*scope.maxMult;
-        scope.curHeight = scope.maxMult > 1 ? h : h*scope.maxMult;
-        scope.reGrid();
-        scope.browserfy(scope.curWidth, scope.curHeight);
-        //TODO if (maxMult < 1) { //resize img };
+        scope.defineNewPicStyle(w, h);
         scope.$apply();
       });
     }
